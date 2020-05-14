9463095e5d4c5a16cba4dfe6fbafa7538f2f573c
Farouk Methkal <faroukmethkal@localhost.localdomain>
2020-05-14 16:13:03 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log("Client side js loaded");
+
+const fetchLocaton = (location) => {
+  messageOne.textContent = "Loading..";
+  messageTwo.textContent = "";
+
+  fetch(`http://localhost:5000/weather?address=${location}`).then((res) => {
+    res.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+};
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+const error = document.querySelector("#error");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  fetchLocaton(location);
+});
---
+++ src/app.js
@@ -0,0 +1,99 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+// Define path for express config
+const publicDirPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath));
+
+//
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Farouk Methkal",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Farouk Methkal",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Farouk Methkal",
+    message: "If you need help you cann contact nfe@gmasil.com",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } ={}) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({ error: "You must provide a search term" });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Farouk Methkal",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Farouk Methkal",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is up on port 5000");
+});
